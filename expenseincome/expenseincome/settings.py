e635b7787f291f38c14dbe237048cadcfe53e246
Sukhvsin2 <sukhvsingh2026@gmail.com>
2020-12-26 21:09:02 +0530

    Pagination Added

--- expenseincome/expenseincome/settings.py
+++ expenseincome/expenseincome/settings.py
@@ -35,6 +35,16 @@ INSTALLED_APPS = [
     'expenses',
 ]
 
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'api_key': {
+            'type': 'apiKey',
+            'in': 'header',
+            'name': 'Authorization'
+        }
+    },
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -80,7 +90,9 @@ REST_FRAMEWORK = {
     'NON_FIELD_ERRORS_KEY': 'error',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
 
 
