5c37832f5c83aa109f34678c8cbdaa2edc237dbc
Sukhvsin2 <sukhvsingh2026@gmail.com>
2021-01-01 14:53:59 +0530

    Some changes for Heroku

--- expenseincome/expenseincome/settings.py
+++ expenseincome/expenseincome/settings.py
@@ -59,6 +59,7 @@ SWAGGER_SETTINGS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -145,6 +146,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Email adding Step
